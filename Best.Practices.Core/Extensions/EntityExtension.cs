9b44f7be04da28a5b1337796580ed6f97b017a1f
Elias da Silva Rita <elias_rita@hotmail.com>
2024-05-05 07:46:22 -0300

    Create entity extensions

--- Best.Practices.Core/Extensions/EntityExtension.cs
+++ Best.Practices.Core/Extensions/EntityExtension.cs
@@ -1,5 +1,7 @@
-﻿using Best.Practices.Core.Domain.Models.Interfaces;
+﻿using Best.Practices.Core.Common;
+using Best.Practices.Core.Domain.Models.Interfaces;
 using Best.Practices.Core.Exceptions;
+using System.Linq.Expressions;
 
 namespace Best.Practices.Core.Extensions
 {
@@ -15,7 +17,17 @@ namespace Best.Practices.Core.Extensions
             return entity;
         }
 
-        public static Entity ThrowsInvalidInputIfEntityExists<Entity>(this Entity entity, string errorMessage) where Entity : IBaseEntity
+        public static IEnumerable<Entity> ThrowResourceNotFoundIfIsNullOrEmpty<Entity>(this IEnumerable<Entity> entities, string errorMessage) where Entity : IBaseEntity
+        {
+            if (entities.IsNullOrEmpty())
+            {
+                throw new ResourceNotFoundException(errorMessage);
+            }
+
+            return entities;
+        }
+
+        public static Entity ThrowInvalidInputIfIsNotNull<Entity>(this Entity entity, string errorMessage) where Entity : IBaseEntity
         {
             if (entity is not null)
             {
@@ -23,6 +35,70 @@ namespace Best.Practices.Core.Extensions
             }
 
             return entity;
+        }
+
+        public static IEnumerable<Entity> ThrowInvalidInputIfIsHasItems<Entity>(this IEnumerable<Entity> entities, string errorMessage) where Entity : IBaseEntity
+        {
+            if (!entities.IsNullOrEmpty())
+            {
+                throw new ResourceNotFoundException(errorMessage);
+            }
+
+            return entities;
+        }
+
+        public static Entity ThrowInvalidInputIfMatches<Entity>(this Entity entity, Predicate<Entity> match, string errorMessage) where Entity : IBaseEntity
+        {
+            if (match(entity))
+            {
+                throw new InvalidInputException(errorMessage);
+            }
+
+            return entity;
+        }
+
+        public static IEnumerable<Entity> ThrowInvalidInputIfMatches<Entity>(this IEnumerable<Entity> entities, Predicate<Entity> match, string errorMessage) where Entity : IBaseEntity
+        {
+            var matchedItems = entities.Where(i => match(i)).ToList();
+
+            if (matchedItems.Count != CommonConstants.QuantityZeroItems)
+            {
+                throw new InvalidInputException(errorMessage);
+            }
+
+            return entities;
+        }
+
+        public static IEnumerable<Entity> ThrowInvalidInputIfHasDuplicates<Entity, T, TKey>(this IEnumerable<Entity> entities, string errorMessage, params Expression<Func<Entity, TKey>>[] keySelectors) where Entity : IBaseEntity
+        {
+            if (entities.HasDuplicates(keySelectors))
+            {
+                throw new InvalidInputException(errorMessage);
+            }
+
+            return entities;
+        }
+
+        public static Entity ThrowInvalidInputIfDoesNotMatch<Entity>(this Entity entity, Predicate<Entity> match, string errorMessage) where Entity : IBaseEntity
+        {
+            if (match(entity))
+            {
+                throw new InvalidInputException(errorMessage);
+            }
+
+            return entity;
+        }
+
+        public static IEnumerable<Entity> ThrowInvalidInputIDoesNotMatc<Entity>(this IEnumerable<Entity> entities, Predicate<Entity> match, string errorMessage) where Entity : IBaseEntity
+        {
+            var matchedItems = entities.Where(i => match(i)).ToList();
+
+            if (matchedItems.Count == CommonConstants.QuantityZeroItems)
+            {
+                throw new InvalidInputException(errorMessage);
+            }
+
+            return entities;
         }
     }
 }
