35b9bd3aeb807b3f1a9c3932861640060dabea49
Elias Silva <166063801+eliassilvadev@users.noreply.github.com>
2024-05-04 23:23:13 -0300

    Create extensions (#38)
    Co-authored-by: Elias da Silva Rita <elias_rita@hotmail.com>

---
+++ Best.Practices.Core/Extensions/EntityExtension.cs
@@ -0,0 +1,28 @@
+﻿using Best.Practices.Core.Domain.Models.Interfaces;
+using Best.Practices.Core.Exceptions;
+
+namespace Best.Practices.Core.Extensions
+{
+    public static class EntityExtension
+    {
+        public static Entity ThrowResourceNotFoundIfIsNull<Entity>(this Entity entity, string errorMessage) where Entity : IBaseEntity
+        {
+            if (entity is null)
+            {
+                throw new ResourceNotFoundException(errorMessage);
+            }
+
+            return entity;
+        }
+
+        public static Entity ThrowsInvalidInputIfEntityExists<Entity>(this Entity entity, string errorMessage) where Entity : IBaseEntity
+        {
+            if (entity is not null)
+            {
+                throw new InvalidInputException(errorMessage);
+            }
+
+            return entity;
+        }
+    }
+}
