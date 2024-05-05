ee30e1ca4dcc470d003fc866d8ecf7f9d6cb2b0b
Elias da Silva Rita <elias_rita@hotmail.com>
2024-05-04 23:21:47 -0300

    Create extensions

--- Best.Practices.Core/Extensions/EnumerableExtension.cs
+++ Best.Practices.Core/Extensions/EnumerableExtension.cs
@@ -1,4 +1,6 @@
-﻿namespace Best.Practices.Core.Extensions
+﻿using System.Linq.Expressions;
+
+namespace Best.Practices.Core.Extensions
 {
     public static class EnumerableExtension
     {
@@ -11,6 +13,30 @@
         public static bool IsNullOrEmpty<T>(this IEnumerable<T> collection)
         {
             return collection == null || !collection.Any();
+        }
+
+        public static bool HasDuplicates<T, TKey>(this IEnumerable<T> source, params Expression<Func<T, TKey>>[] keySelectors)
+        {
+            var uniqueKeys = new HashSet<string>();
+
+            foreach (var item in source)
+            {
+                var keyValues = keySelectors.Select(selector =>
+                {
+                    var compiledSelector = selector.Compile();
+                    return compiledSelector(item);
+                });
+
+                var compositeKey = string.Join("|", keyValues);
+
+
+                if (!uniqueKeys.Add(compositeKey))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
