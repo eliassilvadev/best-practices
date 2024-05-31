fbd893f007524c32b1e958da7bb36420aad0b285
Elias da Silva Rita <elias_rita@hotmail.com>
2024-05-31 11:40:47 -0300

    Create CqrsCommandProviderInMemory

---
+++ Best.Practices.Core/Domain/Cqrs/CommandProviders/CqrsCommandProviderInMemory.cs
@@ -0,0 +1,61 @@
+﻿using Best.Practices.Core.Domain.Models.Interfaces;
+
+namespace Best.Practices.Core.Domain.Cqrs.CommandProviders
+{
+    internal class CqrsCommandProviderInMemory<Entity> : ICqrsCommandProvider<Entity> where Entity : IBaseEntity
+    {
+        private readonly InMemoryConnection<Entity> _connection;
+
+        public CqrsCommandProviderInMemory(InMemoryConnection<Entity> connection)
+        {
+            _connection = connection;
+        }
+
+        public IEntityCommand GetAddCommand(Entity entity)
+        {
+            IList<Entity> entities = GetEntityList();
+
+            entities.Add(entity);
+        }
+
+        public Task<Entity> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected IList<Entity> GetEntityList()
+        {
+            IList<Entity> entidades = _connection.PersistedEntities[typeof(Entity).Name];
+
+            return entidades;
+        }
+
+        public IEntityCommand GetDeleteCommand(Entity entity)
+        {
+            IList<Entity> entities = GetEntityList();
+
+            Entity previousEntity = entities.Where(e => e.Id == entity.Id).FirstOrDefault();
+
+            if (previousEntity is not null)
+            {
+                entities.Remove(previousEntity);
+            }
+
+            return true;
+        }
+
+        public IEntityCommand GetUpdateCommand(Entity entity)
+        {
+            IList<Entity> entities = GetEntityList();
+
+            Entity previousEntity = entities.Where(e => e.Id == entity.Id).FirstOrDefault();
+
+            if ((previousEntity is not null) && (previousEntity.Equals(entity)))
+            {
+                previousEntity.Copy(entity);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Best.Practices.Core/Domain/Cqrs/CommandProviders/InMemoryConnection.cs
@@ -0,0 +1,26 @@
+﻿using Best.Practices.Core.Domain.Models.Interfaces;
+
+namespace Best.Practices.Core.Domain.Cqrs.CommandProviders
+{
+    public class InMemoryConnection<Entity> where Entity : IBaseEntity
+    {
+        private static Dictionary<string, IList<Entity>> _persistedEntities;
+
+        public Dictionary<string, IList<Entity>> PersistedEntities
+        {
+            get
+            {
+                if (!_persistedEntities.ContainsKey(typeof(Entity).Name))
+                {
+                    _persistedEntities[typeof(Entity).Name] = new List<Entity>();
+                }
+
+                return _persistedEntities;
+            }
+            set
+            {
+                _persistedEntities = value;
+            }
+        }
+    }
+}
