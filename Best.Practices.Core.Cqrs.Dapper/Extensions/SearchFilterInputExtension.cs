4151843c8bdce169bdeedc164ceb81052deac0a8
Elias da Silva Rita <elias_rita@hotmail.com>
2024-06-14 21:02:25 -0300

    Refactoring query provider

--- Best.Practices.Core.Cqrs.Dapper/Extensions/SearchFilterInputExtension.cs
+++ Best.Practices.Core.Cqrs.Dapper/Extensions/SearchFilterInputExtension.cs
@@ -1,12 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Best.Practices.Core.Application.Dtos.Input;
+using Best.Practices.Core.Domain.Enumerators;
 
 namespace Best.Practices.Core.Cqrs.Dapper.Extensions
 {
-    public class SearchFilterInputExtension
+    public static class SearchFilterInputExtension
     {
+        public static Dictionary<string, object> GetParameters(this SearchFilterInput filter)
+        {
+            Dictionary<string, object> dictionary = new Dictionary<string, object>();
+            switch (filter.FilterType)
+            {
+                case FilterType.Containing:
+                    dictionary.Add(filter.FilterProperty, "%" + filter.FilterValue + "%");
+                    break;
+                case FilterType.Equals:
+                    dictionary.Add(filter.FilterProperty, filter.FilterValue ?? "");
+                    break;
+                case FilterType.DifferentFrom:
+                    dictionary.Add(filter.FilterProperty, filter.FilterValue ?? "");
+                    break;
+                case FilterType.StartsWith:
+                    dictionary.Add(filter.FilterProperty, filter.FilterValue + "%");
+                    break;
+                case FilterType.EndsWith:
+                    dictionary.Add(filter.FilterProperty, "%" + filter.FilterValue);
+                    break;
+                case FilterType.LessThan:
+                    dictionary.Add(filter.FilterProperty, filter.FilterValue ?? "");
+                    break;
+                case FilterType.LessThanOrEqualTo:
+                    dictionary.Add(filter.FilterProperty, filter.FilterValue ?? "");
+                    break;
+                case FilterType.GreaterThan:
+                    dictionary.Add(filter.FilterProperty, filter.FilterValue ?? "");
+                    break;
+                case FilterType.GreaterThanOrEqualTo:
+                    dictionary.Add(filter.FilterProperty, filter.FilterValue ?? "");
+                    break;
+                case FilterType.Empty:
+                    dictionary.Add(filter.FilterProperty, "");
+                    break;
+                case FilterType.NotEmpty:
+                    dictionary.Add(filter.FilterProperty, "");
+                    break;
+                default:
+                    dictionary.Add(filter.FilterProperty, filter.FilterValue ?? "");
+                    break;
+            }
+
+            return dictionary;
+        }
+
+        public static string GetSqlFilter(this SearchFilterInput filter)
+        {
+            string filterProperty = filter.FilterProperty;
+            return filter.FilterType switch
+            {
+                FilterType.Containing => filterProperty + " LIKE ",
+                FilterType.Equals => filterProperty + " = ",
+                FilterType.DifferentFrom => filterProperty + " <> ",
+                FilterType.StartsWith => filterProperty + " LIKE ",
+                FilterType.EndsWith => filterProperty + " LIKE ",
+                FilterType.LessThan => filterProperty + " < ",
+                FilterType.LessThanOrEqualTo => filterProperty + " <= ",
+                FilterType.GreaterThan => filterProperty + " > ",
+                FilterType.GreaterThanOrEqualTo => filterProperty + " >= ",
+                FilterType.Empty => filterProperty + " = ",
+                FilterType.NotEmpty => filterProperty + " <> ",
+                _ => filterProperty + " = ",
+            } + "@" + filter.FilterProperty;
+        }
     }
 }
