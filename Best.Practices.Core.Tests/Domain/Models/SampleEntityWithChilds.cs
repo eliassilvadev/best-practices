0e1d02ccf9133efedef3f178ce7fb399d63bcdba
Elias Silva <166063801+eliassilvadev@users.noreply.github.com>
2024-06-22 19:31:23 -0300

    Create colection extension (#44)
    Co-authored-by: Elias da Silva Rita <elias_rita@hotmail.com>

---
+++ Best.Practices.Core.Tests/Domain/Models/SampleEntityWithChilds.cs
@@ -0,0 +1,22 @@
+﻿using Best.Practices.Core.Domain.Models;
+using Best.Practices.Core.Domain.Models.Interfaces;
+
+namespace Best.Practices.Core.Tests.Domain.Models
+{
+    public class SampleEntityWithChilds : BaseEntity
+    {
+        public virtual string SampleCode { get; set; }
+        public virtual string SampleName { get; set; }
+        public virtual IEntityList<ChildClassListItem> Childs { get; set; }
+
+        public SampleEntityWithChilds()
+        {
+            Childs = new EntityList<ChildClassListItem>();
+        }
+
+        public void AddChild(ChildClassListItem item)
+        {
+            Childs.Add(item);
+        }
+    }
+}
